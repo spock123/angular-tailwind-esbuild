e8ebcafed700ad2406817b6bf45f13cb08f0154a
Lars Jeppesen <lars.jeppesen@gmail.com>
2023-06-11 22:36:07 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: '',
+  purge: {
+    content: [
+      './src/**/*.{html,ts}',
+    ]
+  },
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
